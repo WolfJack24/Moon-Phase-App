e76853364e78083d3f569b9505ddffe4abe09a61
Liam <liamschroder1@gmail.com>
2024-07-25 12:26:57 +0200

    Add Costants to a seperate file

---
+++ constants.py
@@ -0,0 +1,11 @@
+# pylint: disable=missing-module-docstring, missing-class-docstring
+
+class Constants():
+    def __init__(self):
+        pass
+
+    DATE: str | None = None
+    IMAGE_PATH: str = "images"
+    DEFAULT_DATE: str = "2024-07-18"
+    DEFAULT_STYLE: str = "default"
+    DEFAULT_ORIENTATION: str = "south-up"
